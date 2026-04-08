7f45b521f8def93bc565fb6a07e4363199197d5e
Uday Kapila <udaykapila@Udays-MacBook.local>
2026-04-08 23:50:01 +0530

    connected frontend to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://hospital-backend-q5yd.onrender.com",
 });
 
 // 🔥 attach token automatically
